d8667849ddaa9a3d682732b5cd5bc7659ea38ec6
CK <kok.chee.kean@outlook.com>
2018-07-13 09:28:02 +0800

    fixed

--- index.js
+++ index.js
@@ -9,22 +9,7 @@ app.use(logger('dev'));
 
 
 // Routes
-app.get('/api/v1', (req, res) => {
-    db.query('SELECT * FROM fibonacci LIMIT 1;', (err, result) => {
-        if (err) {
-            console.log(err.message);
-            res.status(503).send({
-                status: "database unavailable",
-                result: null
-            })
-        } else {
-            res.status(200).send({
-                status: "server is up",
-                result: null
-            })
-        }
-    })
-})
+
 
 // Server start
 const server = app.listen(3000);
